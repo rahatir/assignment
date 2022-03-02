aa67fb6673cae024dc029cb11a9191e932196438
rahatir <75853201+rahatir@users.noreply.github.com>
2022-03-02 16:40:40 +0100

    Creation Migrations
    Added Migration (tables) for products and coupons

---
+++ database/migrations/2022_03_02_153653_create_coupons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('code', 10)->default('')->unique();
+            $table->decimal('discount');
+            $table->boolean('used')->default(false);
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
